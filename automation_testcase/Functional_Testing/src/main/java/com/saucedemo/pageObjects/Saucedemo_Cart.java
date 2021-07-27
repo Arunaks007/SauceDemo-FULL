6311e5fb20b1aff0b3dd2ece94772a46a0a72e08
arunr <arunr@DESKTOP-O16ND8T>
2021-07-27 18:09:43 +0530

    Adding new TC

--- automation_testcase/Functional_Testing/src/main/java/com/saucedemo/pageObjects/Saucedemo_Cart.java
+++ automation_testcase/Functional_Testing/src/main/java/com/saucedemo/pageObjects/Saucedemo_Cart.java
@@ -1,5 +1,14 @@
 package com.saucedemo.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+
 public class Saucedemo_Cart {
 
+	WebDriver driver;
+	
+	public Saucedemo_Cart(WebDriver ldriver) {
+		driver = ldriver;
+	}
+	
+	
 }
